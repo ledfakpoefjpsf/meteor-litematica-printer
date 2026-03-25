8c204f78e760a3e9530aa507a4d41b2b51a8a409
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-25 18:23:44 -0500

    Remove commented lines from Addon.java

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -9,18 +9,13 @@ import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger("Meteor Litematica Printer");
-    
-    // THIS LINE IS CRITICAL: It fixes the Printer.java error you got!
     public static final Category CATEGORY = new Category("Printer", Items.ORANGE_CONCRETE.getDefaultStack());
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Litematica Printer with Spectator Plus");
 
-        // Registers the original printer
         Modules.get().add(new Printer());
-        
-        // Registers your new spectator module
         Modules.get().add(new SpectatorModule());
     }
 
