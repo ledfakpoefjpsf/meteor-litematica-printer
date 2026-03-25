7bdce81a0e349d1406719e8da46e34a5b4d6c3b1
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-25 18:24:22 -0500

    Update spectator module player name description

--- src/main/java/com/kkllffaa/meteor_litematica_printer/SpectatorModule.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/SpectatorModule.java
@@ -10,21 +10,21 @@ public class SpectatorModule extends Module {
 
     private final Setting<String> playerName = sgGeneral.add(new StringSetting.Builder()
             .name("player-name")
-            .description("The name of the player to spectate.")
+            .description("Type the name of the player to watch.")
             .defaultValue("")
             .build()
     );
 
     public SpectatorModule() {
-        super(Categories.Player, "spectator-plus", "View the world through another player's eyes.");
+        super(Categories.Player, "spectator-plus", "Watch another player.");
     }
 
     @Override
     public void onActivate() {
-        // In 1.21.4, we check world and player like this
         if (mc.world == null || mc.player == null) return;
 
         PlayerEntity target = null;
+        // Yarn uses .getPlayers()
         for (PlayerEntity player : mc.world.getPlayers()) {
             if (player.getGameProfile().getName().equalsIgnoreCase(playerName.get())) {
                 target = player;
@@ -36,7 +36,7 @@ public class SpectatorModule extends Module {
             mc.setCameraEntity(target);
             info("Spectating: " + target.getGameProfile().getName());
         } else {
-            error("Player not found in render distance!");
+            error("Player not found!");
             this.toggle();
         }
     }
@@ -45,7 +45,7 @@ public class SpectatorModule extends Module {
     public void onDeactivate() {
         if (mc.player != null) {
             mc.setCameraEntity(mc.player);
-            info("Camera reset to player.");
+            info("Camera reset.");
         }
     }
 }
