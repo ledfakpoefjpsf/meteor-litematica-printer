b217b94b8cb581ecb8cae73124299c08d582b326
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-25 18:05:24 -0500

    Refactor Addon class and update initialization logging

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -1,32 +1,26 @@
 package com.kkllffaa.meteor_litematica_printer;
 
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
-	public static final Logger LOG = LogManager.getLogger();
-	public static final Category CATEGORY = new Category("Printer", new ItemStack(Items.PINK_CARPET));
+    public static final Logger LOG = LoggerFactory.getLogger("Meteor Litematica Printer");
 
-	@Override
-	public void onInitialize() {
-		LOG.info("Initializing litematica printer");
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Litematica Printer with Spectator Plus");
 
-		// Modules
-		Modules.get().add(new Printer());
-	}
+        // Keeps your Printer module working
+        Modules.get().add(new Printer());
+        
+        // Adds your new Spectator module
+        Modules.get().add(new SpectatorModule());
+    }
 
     @Override
     public String getPackage() {
         return "com.kkllffaa.meteor_litematica_printer";
     }
-
-	@Override
-	public void onRegisterCategories() {
-		Modules.registerCategory(CATEGORY);
-	}
 }
