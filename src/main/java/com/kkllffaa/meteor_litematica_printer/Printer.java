9e0402acaa76099c97bce35c8f71470360bca399
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-25 19:15:54 -0500

    Delete src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.kkllffaa.meteor_litematica_printer;
-import meteordevelopment.meteorclient.systems.modules.Module;
-
-public class Printer extends Module {
-    public Printer() {
-        super(Addon.CATEGORY, "printer-disabled", "Printer is currently disabled.");
-    }
-}
