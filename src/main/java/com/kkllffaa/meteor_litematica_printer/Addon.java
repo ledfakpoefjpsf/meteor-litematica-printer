3f355c88abf7b0c80aada3a764acb48035d35c0d
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-26 11:00:28 -0500

    Refactor Addon class structure and formatting

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -6,35 +6,29 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Import your modules
 import com.kkllffaa.meteor_litematica_printer.SpectatorModule;
 import com.kkllffaa.meteor_litematica_printer.BowSlayer;
 
 public class Addon extends MeteorAddon {
-public static final Logger LOG = LoggerFactory.getLogger("Spectator Plus");
+    public static final Logger LOG = LoggerFactory.getLogger("Spectator Plus");
 
-```
-// Category (tab in Meteor)
-public static final Category CATEGORY = new Category("Custom");
+    public static final Category CATEGORY = new Category("Custom");
 
-@Override
-public void onInitialize() {
-    LOG.info("Initializing Spectator Plus Addon...");
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Spectator Plus Addon...");
 
-    // Register modules here
-    Modules.get().add(new SpectatorModule());
-    Modules.get().add(new BowSlayer());
-}
+        Modules.get().add(new SpectatorModule());
+        Modules.get().add(new BowSlayer());
+    }
 
-@Override
-public void onRegisterCategories() {
-    Modules.registerCategory(CATEGORY);
-}
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
 
-@Override
-public String getPackage() {
-    return "com.kkllffaa.meteor_litematica_printer";
-}
-```
-
+    @Override
+    public String getPackage() {
+        return "com.kkllffaa.meteor_litematica_printer";
+    }
 }
