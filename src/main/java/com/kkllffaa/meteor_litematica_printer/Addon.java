4f1e7070c7846e65629fa7f7b8b1dd80ecc6cb57
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-26 22:21:47 -0500

    Update Addon.java

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -1,13 +1,31 @@
 package com.kkllffaa.meteor_litematica_printer;
 
-import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Addon {
-    public static void init() {
-        // Register modules with Meteor Client
-        Modules modules = MeteorClient.modules;
-        modules.add(new CreativeSurvivalModule());
-        modules.add(new SpectatorModule());
+public class Addon extends MeteorAddon {
+    public static final Logger LOG = LogManager.getLogger();
+    public static final Category CATEGORY = new Category("Printer", new ItemStack(Items.PINK_CARPET));
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing litematica printer");
+        Modules.get().add(new SurvivalGive());
+        Modules.get().add(new SpectatorModule());
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.kkllffaa.meteor_litematica_printer";
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
     }
 }
